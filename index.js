a401c71f95118603e8886487bf75f7dc8dc2e417
Eclipse <lpskirito7@outlook.com>
2020-07-09 09:46:41 -0400

    finished

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
-"use strict";
+'use strict';
 
+const shoppingList = [];
 
+class ShoppingListItem {
+    constructor(name, checked) {
+        Object.assign(this, { name, checked });
+    }
+}
 
-
-$("#js-shopping-list-form").on('submit',function(e){         
- let text =  $("#shopping-list-entry").val();
- $('.shopping-list').append(`<li>
-  <span class="shopping-item">${text}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle">
-      <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete">
-      <span class="button-label">delete</span>
-    </button>
-  </div>
-</li>`); 
-$("#shopping-list-entry").val('');
- e.preventDefault()
-
-})
-
-$("ul").on('click','.shopping-item-delete',function(){
-  $(this).closest('li').remove()
+$(() => {
+    // find our ui elements
+    const entryField = $('#shopping-list-entry');
+    const ul = $('.shopping-list');
+    // define our rendering
+    const renderShoppingListItem = item => `
+        <li>
+            <span class="shopping-item${item.checked ? ' shopping-item__checked' : ""}">${item.name}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>
+    `;
+    const renderShoppingList = () => ul.html(shoppingList.map(renderShoppingListItem).join(''));
+    // read the server-generated list
+    // CONTRACT -- each <li> has one .shopping-item (which may also be .shopping-item__checked)
+    {
+        ul.children().each(function() { // jquery's each() customizes "this" so needs an explicit function()
+            let itemNode = $(this).find('.shopping-item');
+            shoppingList.push(new ShoppingListItem(itemNode.text(), itemNode.hasClass('shopping-item__checked')));
+        });
+    }
+    // handle new items
+    $('#js-shopping-list-form').submit(e => {
+        e.preventDefault(); // block navigation
+        let newItemName = entryField.val();
+        if(!newItemName)
+            return;
+        // TODO -- asynchronously push update to server
+        shoppingList.push(new ShoppingListItem(newItemName, false));
+        renderShoppingList();
+        entryField.val('');
+    });
+    // handle checking and deleting
+    ul.on('click', '.shopping-item-toggle', e => {
+        // TODO -- asynchronously push update to server
+        const item = shoppingList[$(e.target).closest('li').index()];
+        item.checked = !item.checked;
+        renderShoppingList();
+    });
+    ul.on('click', '.shopping-item-delete', e => {
+        // TODO -- asynchronously push update to server
+        shoppingList.splice($(e.target).closest('li').index(), 1);
+        renderShoppingList();
+    })
 });
-$("ul").on('click','.shopping-item-toggle',function(){
-  $(this).closest('li').find(".shopping-item").toggleClass("shopping-item__checked")
-})
 
